35e718bdba3a05e7f592c2a804393bb0843f30cb
Louis <thatcherlg@cardiff.ac.uk>
2022-07-02 00:25:13 +0100

    dark mode background

--- serverless/components/layout.js
+++ serverless/components/layout.js
@@ -4,9 +4,9 @@ import Footer from './footer'
 
 export default function Layout({ children }) {
     return (
-        <div className="flex flex-col h-screen justify-between">
+        <div className="flex flex-col h-screen justify-between dark:bg-gray-800">
             <Navbar className="h-10 sticky"/>
-            <main className="mb-auto top-20 bg-amber-300">{children}</main>
+            <main className="mb-auto top-20">{children}</main>
         </div>
     )
 }
